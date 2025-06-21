d61f69380a41976bfe9aab632d0900cfd880dad4
ineuron <learnwithhimo@gmail.com>
2025-06-21 16:08:43 +0530

    Fix: Build issues and configuration updates

--- src/test/java/com/example/buy_buddy_backend/BuyBuddyBackendApplicationTests.java
+++ src/test/java/com/example/buy_buddy_backend/BuyBuddyBackendApplicationTests.java
@@ -2,12 +2,12 @@ package com.example.buy_buddy_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BuyBuddyBackendApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
